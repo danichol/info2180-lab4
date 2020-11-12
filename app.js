badaf855cb6e12edac8223826fe1bbc4d91a313b
Daneel Nichol <daneel.nichol@mymona.uwi.edu>
2020-11-11 21:15:21 -0500

    Modified

--- app.js
+++ app.js
@@ -1,26 +1,66 @@
-<script type="text/javascript">
+window.onload=myfunction;
 
-    $(document).ready(function () { // Make sure the elements are loaded on the page
-        // Listen for a click event on the button
-        $('#srch').click(favfunct);
-    });
+function myfunction(){
+  let buttn = document.getElementById("btn");
 
-    function favfunct(e) {
-        // Stop the page from "following" the button (ie. submitting the form)
-        e.preventDefault();
-        e.stopPropagation();
+  searchbtn.addEventListener('click', async function(element) {
+    element.preventDefault();
 
-        // Call an AJAX function to the proper page
-        $.ajax("js/superheroes.php", {
-            // Pass our data to the server
-            data: { "get" : "runfunction", "action" : "favorites1" },
-            // Pass using the appropriate method
-            method: "POST",
-            // When the request is completed and successful, run this code.
-            success: function (response) {
-            // Successfully added to favorites. JS code goes here for this condition.
-                alert ("successfully loaded")
-            }          
-        });
+    var hname = document.getElementsByClassName("name")[0];
+    var search = document.getElementById("superhero").value;
+    var bio = document.getElementsByClassName("biography")[0];
+    var alias = document.getElementsByClassName("alias")[0];
+    var msg = document.getElementsByClassName("message")[0];
+
+    if (heroform === ''){
+      fetch("superheroes.php")
+
+      .then(response => {
+        if (response.ok) {
+          return response.text()
+      } else {
+          return Promise.reject('Something went wrong!')
+      }
+  })
+
+      .then(data=> {
+        msg.innerHTML = data;
+        hname.innerHTML = '' ;
+        alias.innerHTML = '';
+        bio.innerHTML = '';
+      })
+
+      .catch(error => console.log('There was an error: ' + error));
     }
-</script>
+    else{
+      fetch("superheroes.php",{
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(heroform)
+      })
+
+      .then(response => {
+        if (response.ok) {
+          return response.text()
+      } else {
+          return Promise.reject('something went wrong!')
+      }
+      })
+
+      .then(data => {
+        var hero = JSON.parse(data);
+        if (typeof(hero) === "string"){
+            bio.innerHTML = data ;
+        } else if (typeof(hero) === "object"){
+            var heroname = hero["name"];
+            var aliasname = `a.k.a ${hero["alias"]}`;
+            var biography = hero["biography"];
+            msg.innerHTML = '';
+            name.innerHTML = hname ;
+            alias.innerHTML = aname;
+            bio.innerHTML = biography;
+        }
+      });
+    }
+      });
+    }
